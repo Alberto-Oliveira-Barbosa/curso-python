4bb6107b03cecea598ef7df083b87cfde8a31ef4
unknown <albertobarbosa115@gmail.com>
2022-10-24 00:54:03 -0300

    Criada estrutura da pasta e adicionada aulas 1 e 2

---
+++ aulas/aula01.py
@@ -0,0 +1,15 @@
+# Comentários em python
+
+# comentário de 1 linha 
+print('linha 1')
+print('linha 2')
+print('linha 3')  # outro comentário de linha 
+print('linha 4')
+print('linha 5')
+print('linha 6')
+
+"""
+Comentário 
+de 
+multiplas linhas, normalmente 
+"""
---
+++ aulas/aula02.py
@@ -0,0 +1,18 @@
+"""
+Aula 02 - Comando print
+
+print - Função built-in do python que escreve algo na tela
+"""
+print(1234)
+print('nome1', 'nome2')
+
+# modificando a forma de  exibição da função
+print('nome1', 'nome2', sep='-')
+
+# modificando a forma de  exibição da função
+print('nome1', 'nome2', sep='-', end='')
+print('nome3')
+
+# como exercício criar uma mascára de exibição do CPF 999.999.999-99
+print(999,999,999, sep='.',end='-')
+print(99)
